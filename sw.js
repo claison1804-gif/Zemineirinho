86c16bb80fae96898d8289f8596127ea1fffb57e
claison1804-gif <claison1804@gmail.com>
2026-09-17 14:00:30 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ const APP_SHELL = [
   './',
   './index.html',
   './manifest.webmanifest',
-  './icon-192.svg',
-  './icon-512.svg'
+  './icon-192.png',
+'./icon-512.png'
 ];
 
 self.addEventListener('install', event => {
